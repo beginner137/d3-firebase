45726c8f14c6d62ea617b08557fea7c26830eaf7
Doris Xiang <doris.xiang@revelsystems.com>
2019-04-01 19:37:48 -0400

    Section 3

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-const canvas = d3.select(".canvas");
-const svg = canvas
-  .append("svg")
-  .attr("width", 600)
-  .attr("height", 600);
+const svg = d3.select("svg");
 
-const group = svg.append("g").attr("transform", "translate(0, 100)");
-group
-  .append("rect")
-  .attr("width", 200)
-  .attr("height", 100)
-  .attr("fill", "blue")
-  .attr("x", 20)
-  .attr("y", 20);
+d3.json("planets.json").then(data => {
+  const circs = svg.selectAll("circle").data(data);
 
-group
-  .append("circle")
-  .attr("r", 50)
-  .attr("cx", 300)
-  .attr("cy", 70)
-  .attr("fill", "pink");
+  circs
+    .attr("cy", 200)
+    .attr("cx", d => d.distance)
+    .attr("r", d => d.radius)
+    .attr("fill", d => d.fill);
 
-group
-  .append("line")
-  .attr("x1", 370)
-  .attr("x2", 400)
-  .attr("y1", 20)
-  .attr("y2", 120)
-  .attr("stroke", "red");
-
-svg
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 200)
-  .attr("fill", "grey")
-  .text("hello")
-  .style("font-family", "arial");
+  circs
+    .enter()
+    .append("circle")
+    .attr("cy", 200)
+    .attr("cx", d => d.distance)
+    .attr("r", d => d.radius)
+    .attr("fill", d => d.fill);
+});
