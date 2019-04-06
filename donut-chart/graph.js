dba9f89025fac8fed0697ee087999d8f6adb3bdb
Doris Xiang <dorisxiang@gatech.edu>
2019-04-06 14:14:37 -0400

    section 9

--- donut-chart/graph.js
+++ donut-chart/graph.js
@@ -36,6 +36,19 @@ const legend = d3
   .shape("circle")
   .shapePadding(10)
   .scale(colour);
+
+const tip = d3
+  .tip()
+  .attr("class", "tip card")
+  .html(d => {
+    let content = `<div class="name">${d.data.name}</div>`;
+    content += `<div class="cost">${d.data.cost}</div>`;
+    content += `<div class="delete">Click slice to delete</div>`;
+    return content;
+  });
+
+graph.call(tip);
+
 // update function
 const update = data => {
   colour.domain(data.map(d => d.name));
@@ -76,6 +89,18 @@ const update = data => {
     .transition()
     .duration(750)
     .attrTween("d", arcTweenEnter);
+
+  graph
+    .selectAll("path")
+    .on("mouseover", (d, i, n) => {
+      tip.show(d, n[i]);
+      handleMouseOver(d, i, n);
+    })
+    .on("mouseout", (d, i, n) => {
+      tip.hide();
+      handleMouseOut(d, i, n);
+    })
+    .on("click", handleClick);
 };
 
 // data array and firestore
@@ -134,3 +159,26 @@ function arcTweenUpdate(d) {
     return arcPath(i(t));
   };
 }
+
+//event handlers
+const handleMouseOver = (d, i, n) => {
+  d3.select(n[i])
+    //name the transition so that it doesn't affect other transitions
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", "#fff");
+};
+
+const handleMouseOut = (d, i, n) => {
+  d3.select(n[i])
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", colour(d.data.name));
+};
+
+const handleClick = d => {
+  const id = d.data.id;
+  db.collection("expenses")
+    .doc(id)
+    .delete();
+};
